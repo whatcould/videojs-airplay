1f6e555f8cade20c2cbd718b244e35dd7e8d49cd
David Reese <david@whatcould.com>
2020-09-17 12:37:54 -0400

    Remove underscore dependency

--- src/js/enableAirPlay.js
+++ src/js/enableAirPlay.js
@@ -4,9 +4,7 @@
  * @module enableAirPlay
  */
 
-var hasAirPlayAPISupport = require('./lib/hasAirPlayAPISupport'),
-    _ = require('underscore');
-
+var hasAirPlayAPISupport = require('./lib/hasAirPlayAPISupport');
 /**
  * @private
  * @param {object} the Video.js Player instance
@@ -93,8 +91,10 @@ function enableAirPlay(player, options) {
  */
 module.exports = function(videojs) {
    videojs.registerPlugin('airPlay', function(options) {
-      var pluginOptions = _.extend({ addButtonToControlBar: true }, options || {});
-
+     var pluginOptions =  options || {};
+     if(pluginOptions.addButtonToControlBar == null ) {
+       pluginOptions.addButtonToControlBar = true;
+     }
       // `this` is an instance of a Video.js Player.
       // Wait until the player is "ready" so that the player's control bar component has
       // been created.
